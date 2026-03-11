f63c9851778ce3261ece709021a7811de96ad335
arakakihiyori <hiyori3tamashiro@icloud.com>
2026-03-11 12:49:42 +0900

    課題提出

---
+++ src/curriculum4/Question5.java
@@ -0,0 +1,13 @@
+package curriculum4;
+
+public class Question5 { // 1. ファイル名と同じにする
+    public static void main(String[] args) { // 3. カッコを閉じる
+        int a = 5;
+        int b = 6;
+
+        System.out.println(a); // 2. Sを大文字にする
+        System.out.println(b);
+    }
+}
+
+
